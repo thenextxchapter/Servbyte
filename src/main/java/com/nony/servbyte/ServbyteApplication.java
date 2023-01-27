580717d3dbf80cb0c400c729a8c6682433f46981
chinonso ata <chinonso.ata@gmail.com>
2023-01-27 10:36:00 +0100

    init

---
+++ src/main/java/com/nony/servbyte/ServbyteApplication.java
@@ -0,0 +1,13 @@
+package com.nony.servbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServbyteApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServbyteApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/nony/servbyte/ServbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nony.servbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServbyteApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
